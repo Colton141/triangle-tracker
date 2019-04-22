03899cae6252ff86784a7a0e48c2aed8e4651f72
AJ Ancheta and Colton Lacey <aj@>
2019-04-22 13:08:29 -0700

    add functionality to a trangle tracker program

---
+++ Triangles.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections.Generic;
+
+class Triangle
+{
+  static void Main()
+  {
+    Console.WriteLine("Enter a number for one side of a triangle");
+    string inputSideOne = Console.ReadLine();
+    int sideOne = int.Parse(inputSideOne);
+
+    Console.WriteLine("Enter a second number for another side of the triangle");
+    string inputSideTwo = Console.ReadLine();
+    int sideTwo = int.Parse(inputSideTwo);
+
+
+    Console.WriteLine("Enter a third number for the last side of the triangle");
+    string inputSideThree = Console.ReadLine();
+    int sideThree = int.Parse(inputSideThree);
+
+    if (sideOne==sideTwo && sideTwo==sideThree)
+      {
+         Console.WriteLine("equilateral");
+      } else if (sideOne==sideTwo || sideOne==sideThree || sideThree==sideTwo)
+      {
+        Console.WriteLine("isosceles");
+      } else if (sideOne <= (sideTwo + sideThree) && sideTwo <= (sideThree + sideOne) && sideThree <= (sideOne + sideTwo))
+      {
+        Console.WriteLine("not a triangle");
+      }
+       else
+      {
+        Console.WriteLine("scalene");
+      }
+  }
+}
